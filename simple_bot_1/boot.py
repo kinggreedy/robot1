f8e11e8a33b38b759652b12dc1d2f053d233a8da
Chau Nguyen <6055140+kinggreedy@users.noreply.github.com>
2026-06-12 20:11:39 +0300

    Removed boot.py logic for bot 1

--- simple_bot_1/boot.py
+++ simple_bot_1/boot.py
@@ -1,8 +1,8 @@
-import os
-import board
-import digitalio
-import storage
+# import os
+# import board
+# import digitalio
+# import storage
 
-vbus = digitalio.DigitalInOut(board.GP24)
-vbus.direction = digitalio.Direction.INPUT
-storage.remount("/", readonly=vbus.value)
+# vbus = digitalio.DigitalInOut(board.GP24)
+# vbus.direction = digitalio.Direction.INPUT
+# storage.remount("/", readonly=vbus.value)
